ef853976547ec54e99e9802d2cdccfba4e791ae1
Joe Schoonover <joe@fluidnumerics.com>
2020-12-04 17:25:43 +0000

    Add Intel Compilers via OneAPI yum repository. Remove AOMP
    * WRF does not support builds with Clang/Flang presently

--- wrf-install.sh
+++ wrf-install.sh
@@ -3,9 +3,33 @@
 
 SLURM_VER="20-02"
 
+function install_intel_oneapi {
+cat >/etc/yum.repos.d/oneAPI.repo <<EOF
+[oneAPI]
+name=Intel(R) oneAPI repository
+baseurl=https://yum.repos.intel.com/oneapi
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+EOF
+
+yum update -y
+yum install -y intel-basekit intel-hpckit
+
+cat > /etc/profile.d/oneapi.sh <<EOF
+#!/bin/bash
+
+source /opt/intel/oneapi/setvars.sh
+EOF
+
+
+}
+
 function source_packages {
   source /apps/spack/share/spack/setup-env.sh
   source /etc/profile.d/cuda.sh
+  source /opt/intel/oneapi/setvars.sh
 }
 
 function install_lustre_client {
@@ -50,20 +74,19 @@ function install_spack {
 
   source_packages
 
+  cp ./spack/packages.yaml /apps/spack/etc/spack/packages.yaml
+
   spack compiler find --scope site
 
+  spack install gcc@10.2.0
+  spack load gcc@10.2.0
+  spack compiler find --scope site
+  spack unload gcc@10.2.0
 }
 
 function install_additional_systempackages {
-  yum install -y libseccomp-devel
-}
-
-function install_aomp {
-
-  # Install ROCm AOMP for clang, flang, and clang-cpp
-  wget https://github.com/ROCm-Developer-Tools/aomp/releases/download/rel_11.11-2/aomp_REDHAT_7-11.11-2.x86_64.rpm
-  rpm -i aomp_REDHAT_7-11.11-2.x86_64.rpm
-
+  yum install -y libseccomp-devel \
+                 csh
 }
 
 function setup_wrf_envs {
@@ -72,7 +95,7 @@ function setup_wrf_envs {
 
   # GCC + OpenMPI
   spack env create wrf_gcc_openmpi
-  cp spack/wrf_gcc_openmpi/spack.yaml /apps/spack/environments/wrf_gcc_openmpi/spack.yaml
+  cp spack/wrf_gcc_openmpi/spack.yaml /apps/spack/var/spack/environments/wrf_gcc_openmpi/spack.yaml
   spack env activate wrf_gcc_openmpi
   spack concretize
 
@@ -84,9 +107,9 @@ install_additional_systempackages
 
 install_lustre_client
 
-install_spack
+install_intel_oneapi
 
-install_aomp
+install_spack
 
 setup_wrf_envs
 
